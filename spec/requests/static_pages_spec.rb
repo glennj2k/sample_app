6bb3f9f8106b9e449f4b0f9faad1f7bd7d9eaaea
glennj2k <glennj2k@gmail.com>
2012-06-01 03:22:31 -0700

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "StaticPages" do
       page.should have_content('Sample App')
       #response.status.should be(200)
     end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "| Home")
+    end
   end
   
   describe "Help page" do
@@ -15,12 +20,22 @@ describe "StaticPages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "| Help")
+    end
   end
   
   describe "About page" do
     it "should have the content ('About')" do
-      visit 'static_pages/about'
+      visit '/static_pages/about'
       page.should have_content('About')
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                        :text => "| About")
     end
   end
 end
